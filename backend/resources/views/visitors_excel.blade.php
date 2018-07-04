67096f2e7e0ce78a9eb3c02ce43d64067ea20042
joseivansandoya <jisandoya@gmail.com>
2018-07-03 20:26:20 -0500

    update visitors excel report

--- backend/resources/views/visitors_excel.blade.php
+++ backend/resources/views/visitors_excel.blade.php
@@ -9,6 +9,9 @@
             <th>Nombre</th>
             <th>Apellido</th>
             <th>Email</th>
+            <th>Género</th>
+            <th>Teléfono</th>
+            <th>Tipo de pisada</th>
             <th>Pto. Venta</th>
             <th>Cod. Vendedor</th>
             <th>Vendedor</th>
@@ -22,6 +25,23 @@
                 <td>{{ $visitor->name }}</td>
                 <td>{{ $visitor->surname }}</td>
                 <td>{{ $visitor->email }}</td>
+                <td>
+                    @if($visitor->gender == '0')
+                    Masculino
+                    @else
+                    Femenino
+                    @endif
+                </td>
+                <td>{{ $visitor->phone }}</td>
+                <td>
+                    @if($visitor->footprint == '0')
+                        Pie cavo
+                    @elseif($visitor->footprint == '1')
+                        Pie medio
+                    @elseif($visitor->footprint == '2')
+                        Pie plano
+                    @endif
+                </td>
                 <td>{{ $visitor->branch->name }}</td>
                 @if(count($visitor->employee) > 0)
                     <td>{{ $visitor->employee->code }}</td>
